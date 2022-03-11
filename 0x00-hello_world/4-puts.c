26815af90b1e0f97d0f83613badb03ce0de712ab
Hetty2021 <henrietta.ampofo@azubiafrica.org>
2022-03-10 20:10:59 -0800

    compile C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints sentence "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, else no zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
