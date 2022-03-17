076117e2245fd5bcef8238f0583a09adac185ec3
Hetty2021 <henrietta.ampofo@azubiafrica.org>
2022-03-17 13:47:13 -0700

    m

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,7 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
-© 2022 GitHub, Inc.
-Terms
